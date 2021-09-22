d3e9e483530cc0e805635588b46df0c7c8183e44
mevlutince <83275992+mevlutince@users.noreply.github.com>
2021-09-23 01:08:17 +0300

    proje tamamlandı

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./styles.css";
+import {connect} from "react-redux"
+import {listeyeEkle,isaretle,temizle} from "./actions"
 
-function App() {
+   const App=(props)=> {
+ 
+  
+    const [text, setText] = useState("");
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yapılacaklar Listesi</h1>
+      <div className="ekleme_formu">
+        <input placeholer="listeye ekle" value={text} onChange={e=>setText(e.target.value)}/>
+        <button onClick={()=>{ props.listeyeEkle(text); setText(""); }}>Ekle</button>
+      </div>
+      <div className="liste">
+        { props.liste.map(item => (
+          <div onClick={()=>props.isaretle(item.id)} className={item.tamamlandi ? "yapildi" : ""} key={item.id}>
+            {item.baslik}
+          </div>
+        )) }
+      </div>
+      <button onClick={()=>props.temizle()} className="temizle">Tamamlananları Temizle</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps=state=>{
+  return{
+    liste:state.liste
+  }
+}
+
+export default connect(mapStateToProps,{listeyeEkle,isaretle,temizle})(App)
